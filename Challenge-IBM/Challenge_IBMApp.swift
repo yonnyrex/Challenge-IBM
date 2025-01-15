e9e748efc181ed341e3737637a0e56a7501d9fe8
Yonny <yonnyriveraespinoza@gmail.com>
2025-01-14 23:37:05 -0500

    Initial Commit

---
+++ Challenge-IBM/Challenge_IBMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_IBMApp.swift
+//  Challenge-IBM
+//
+//  Created by Yonny on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_IBMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge-IBM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge-IBM
+//
+//  Created by Yonny on 14/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
